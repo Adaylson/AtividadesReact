de51fd43394f10bf58274cdf296855b225a62261
Adaylson Arcanjo <adaylsonevaristo15@gmail.com>
2024-05-10 19:20:15 -0300

    Configurações-Contador

--- listadetarefas/src/contador.jsx
+++ listadetarefas/src/contador.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 
 const Contador = () => {
     const[cont, setCont] = useState(0)
+    const[qtdCont, setQtdCont] = useState(1)
+    const[qtdContD, setQtdContD] = useState(1)
 
     function Adicionar(){
-        const somar = cont + 1
+        const somar = cont + qtdCont
         setCont(somar)
     }
     function Subtrair(){
-        const sub = cont - 1
+        const sub = cont - qtdContD
         setCont(sub)
     }
     return(
@@ -21,6 +23,13 @@ const Contador = () => {
                     <button onClick={Adicionar}>Adicionar</button>
                     <button onClick={()=>setCont(0)}>Zerar contador</button>
                 </div>
+            </div>
+            <div>
+                <h2>Configurações</h2>
+                Quantidade a ser somada por click
+                <input type="number" value={qtdCont} onChange={(e)=>setQtdCont(parseInt(e.target.value))}/><br />
+                Quantidade a ser subtraida por click
+                <input type="number" value={qtdContD} onChange={(e)=>setQtdContD(parseInt(e.target.value))}/>
             </div>
         </div>
     )
